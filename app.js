32a93fd74fafdf972f838b8e866230fc33b37f96
00mab <ayan.butt@antino.com>
2024-12-16 13:01:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
-
+const port = process.env.PORT || 3000;  
 // Middleware to handle requests
 app.get('/', (req, res) => {
   res.send('Hello, World! This is the demo app running on port 80.');
